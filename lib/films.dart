750b2ef351c37b8885f853beb7fa408bf24e39ab
Elizabeth Ajileye <ajioluwaseun1@gmail.com>
2024-06-27 18:26:07 +0100

    my commit

---
+++ lib/films.dart
@@ -0,0 +1,29 @@
+import 'package:riverpod_project_6/main.dart';
+
+const allFilms = [
+  Film(
+    id: '1',
+    title: "The Shawshank Redemption",
+    description: "Description for The Shawshank Redemption",
+    isFavorite: false,
+  ),
+  Film(
+    id: '2',
+    title: "The Godfather",
+    description: "Description for The Godfather",
+    isFavorite: false,
+  ),
+  Film(
+    id: '3',
+    title: "The Godfather: Part II",
+    description: "Description for The Godfather Part II",
+    isFavorite: false,
+  ),
+  Film(
+    id: '4',
+    title: "The Dark Knight",
+    description: "Description for The Dark Knight",
+    isFavorite: false,
+  ),
+];
+
--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,48 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
       home: const HomePage(),
     );
   }
+}
+
+@immutable
+class Film {
+  final String id;
+  final String title;
+  final String description;
+  final bool isFavorite;
+
+  const Film(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.isFavorite});
+
+  Film copy({required bool isFavorite}) => Film(
+        id: id,
+        title: title,
+        description: description,
+        isFavorite: isFavorite,
+      );
+
+  @override
+  String toString() => 'Film(id: $id,'
+      'title: $title,'
+      'description: $description,'
+      'isFavorite: $isFavorite,)';
+
+  @override
+  bool operator ==(covariant Film other) =>
+      id == other.id && isFavorite == other.isFavorite;
+
+  @override
+  int get hashCode => Object.hashAll([
+        id,
+        isFavorite,
+      ]);
 }
 
 class HomePage extends ConsumerWidget {
@@ -25,7 +64,7 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: const Color.fromARGB(66, 59, 58, 58),
         title: Text('Films'),
         centerTitle: true,
       ),
